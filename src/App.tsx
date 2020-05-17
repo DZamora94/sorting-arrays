0bcf1892608bb6ae3ac675efe4e4f5e106993256
David <d.zamoraballesteros@gmail.com>
2020-05-17 12:19:50 +0200

    feat(Sorter): logic outside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,13 @@
-import React, { Component } from 'react';
-import ColumnList from './Array/ColumnList';
+import React from 'react';
+import Sorter from './Sorter/Sorter';
 import './App.css';
 
-class App extends Component {
-
-  state = {
-    arrayToSort: [],
-    sortedArray: [],
-  };
-
-  public handleChangeInput(event: any) {
-    const array = [];
-    for (let index = 0; index < event.target.value; index++) {
-      const value: number = parseFloat(Math.random().toFixed(4));
-      array.push(value);
-    }
-    this.setState({arrayToSort: array, sortedArray: array});
-  }
-
-  public bubbleSort(array: number[]) {
-    let done = false;
-    while (!done) {
-      done = true;
-      for (let i = 1; i < array.length; i++) {
-        if (array[i - 1] > array[i]) {
-          done = false;
-          let biggerVal = array[i - 1];
-
-          array[i - 1] = array[i];
-          array[i] = biggerVal;
-
-          this.setState({ sortedArray: array })
-        }
-      }
-    }
-  }
-
-  public unSort() {
-    this.setState({ sortedArray: this.state.arrayToSort })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <input type="number" onChange={(e) => this.handleChangeInput(e)} />Number of values
-        <button onClick={() => this.bubbleSort([...this.state.arrayToSort])}>Click me to Sort</button>
-        <button onClick={() => this.unSort()}>Unsort</button>
-        <div className="List-container">
-          <ColumnList array={this.state.sortedArray} width={this.state.sortedArray.length}></ColumnList>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Sorter></Sorter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sorter/Sorter.tsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import ColumnList from '../Array/ColumnList';
+
+class Sorter extends Component {
+
+    state = {
+        arrayToSort: [],
+        sortedArray: [],
+    };
+
+    public handleChangeInput(event: any) {
+        const array = [];
+        for (let index = 0; index < event.target.value; index++) {
+            const value: number = parseFloat(Math.random().toFixed(4));
+            array.push(value);
+        }
+        this.setState({ arrayToSort: array, sortedArray: array });
+    }
+
+    public bubbleSort(array: number[]) {
+        let done = false;
+        while (!done) {
+            done = true;
+            for (let i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    done = false;
+                    let biggerVal = array[i - 1];
+
+                    array[i - 1] = array[i];
+                    array[i] = biggerVal;
+
+                    this.setState({ sortedArray: array })
+                }
+            }
+        }
+    }
+
+    public unSort() {
+        this.setState({ sortedArray: this.state.arrayToSort })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="number" step="10" onChange={(e) => this.handleChangeInput(e)} />Number of values
+                <button onClick={() => this.bubbleSort([...this.state.arrayToSort])}>Click me to Sort</button>
+                <button onClick={() => this.unSort()}>Unsort</button>
+                <div className="List-container">
+                    <ColumnList array={this.state.sortedArray} width={this.state.sortedArray.length}></ColumnList>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Sorter;
